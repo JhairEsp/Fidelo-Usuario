9aa5b6b9dc7a33c6555019be5c13e3820050a7c8
JhairEsp <126715508+JhairEs@users.noreply.github.com>
2023-03-31 19:52:39 -0500

    cambios

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Register());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Register1());
   }
 }
